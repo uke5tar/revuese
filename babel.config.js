1ee281cea398a5a8939eb5c0377e3b1f11c6ab17
jannis.strehl <jannis.strehl@olx.com>
2019-12-28 20:08:17 +0300

    add babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  presets: [
+module.exports = (api) => {
+  api.cache(true);
+
+  const presets = [
     '@vue/cli-plugin-babel/preset',
-  ],
+  ];
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
