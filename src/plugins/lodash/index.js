9a0d4ff83ca7f9cf6e1899e5082f85100615ace5
jannis.strehl <jannis.strehl@olx.com>
2020-05-10 22:52:50 +0200

    add mdi font add lodash

---
+++ src/plugins/lodash/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-shadow */
+import Vue from 'vue';
+
+/*
+    How to use:
+    1. import lodash methods here via `import {method} from 'lodash/{method}`
+    2. add it to customLodash Object
+    3. use it in the app via `$this._.{method}`
+*/
+const customLodash = {};
+
+const lodash = {
+  install(Vue) {
+    Object.defineProperty(Vue.prototype, '_', { value: customLodash });
+  },
+};
+
+Vue.use(lodash);
+
+export default lodash;
