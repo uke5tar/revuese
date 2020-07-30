f68238a91ffd577a139011da2132520215c399d6
jannis.strehl <jannis.strehl@olx.com>
2020-07-30 22:06:25 +0200

    Add explanation text

--- src/pages/authenticated/Authenticated.vue
+++ src/pages/authenticated/Authenticated.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Only visible for logged in users (authenticated).</div>
+  <div>
+    <p>Only visible for logged in users (authenticated).</p>
+    <p>
+      The app is by default not behind a login-wall.
+      Routes that require authentication need to be added to 'pagesRequireAuth'
+      within helper.js.
+      If the page should additionally appear within the menu they require the
+      attribute 'auth: true' (see authenticated within navigationItems.js).
+    </p>
+  </div>
 </template>
 
 <script>
--- src/pages/guest/Guest.vue
+++ src/pages/guest/Guest.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Only visible for users with role: 'guest' (default)</div>
+  <div>
+    <p>Only visible for users with role: 'guest' (default).</p>
+    <p>
+      Every user receives by default the role 'guest' after signup.
+      If a page should be only accessible for certain roles you need to add the required role
+      to the page within navigationItems.js (see guest as an example). By default every user receives
+      at signup the role 'guest'.
+    </p>
+  </div>
 </template>
 
 <script>
